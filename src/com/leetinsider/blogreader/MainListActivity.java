2a915e461246dae93ce24eafd9a2d2dee37025d9
Andrew Pritykin <apritykin@gmail.com>
2014-06-23 17:19:29 -0400

    Pulled JSON to list Blog Post Titles

--- src/com/leetinsider/blogreader/MainListActivity.java
+++ src/com/leetinsider/blogreader/MainListActivity.java
@@ -1,28 +1,63 @@
 package com.leetinsider.blogreader;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.ListActivity;
-import android.content.res.Resources;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 public class MainListActivity extends ListActivity {
 	
-	protected String[] mAndroidNames;
+	protected String[] mBlogPostTitles;
+	public static final int NUMBER_OF_POSTS = 20;
+	public static final String TAG = MainListActivity.class.getSimpleName();
+	protected JSONObject mBlogData;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_list);
 		
-		Resources resources = getResources();
-		mAndroidNames = resources.getStringArray(R.array.android_names);
-		
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mAndroidNames);
-		setListAdapter(adapter);
+		//Check to see if there is network. If so update app with Blog posts. If not return no network
+		if(isNetworkAvailable()){
+			GetBlogPostsTask getBlogPostsTask = new GetBlogPostsTask();
+			getBlogPostsTask.execute();	
+		}	
+		else{
+			Toast.makeText(this, "Network is unavailable!", Toast.LENGTH_LONG).show();
+		}
 		
 		//Toast.makeText(this, getText(R.string.no_items), Toast.LENGTH_LONG).show();
+	}
+
+	private boolean isNetworkAvailable() {
+		ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+		
+		boolean isAvailable = false;
+		if (networkInfo != null && networkInfo.isConnected()){
+			isAvailable = true;
+		}
+		return isAvailable;
 	}
 
 	@Override
@@ -31,6 +66,78 @@ public class MainListActivity extends ListActivity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main_list, menu);
 		return true;
+	}
+
+	public void updateList() {
+		if (mBlogData == null){
+			//TODO Handle Error
+		}
+		else{
+			try {
+				JSONArray jsonPosts = mBlogData.getJSONArray("posts");
+				mBlogPostTitles = new String[jsonPosts.length()];
+				for(int i =0; i < jsonPosts.length(); i++){
+					JSONObject post = jsonPosts.getJSONObject(i);
+					String title = post.getString("title");
+					title = Html.fromHtml(title).toString();
+					mBlogPostTitles[i] = title;
+				}
+				
+				ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mBlogPostTitles);
+				setListAdapter(adapter);
+			} 
+			catch (JSONException e) {
+				Log.e(TAG, "Excpetion Caught", e);
+			}
+		}
+		
+	}
+	
+	private class GetBlogPostsTask extends AsyncTask<Object, Void, JSONObject>{
+
+		@Override
+		protected JSONObject doInBackground(Object... params) {
+			int responseCode = -1;
+			JSONObject jsonResponse = null;
+			try {
+				URL blogFeedUrl = new URL("http://blog.teamtreehouse.com/api/get_recent_summary/?count=" + NUMBER_OF_POSTS);
+				HttpURLConnection connection = (HttpURLConnection) blogFeedUrl.openConnection();
+				connection.connect();
+				
+				responseCode = connection.getResponseCode();
+				if (responseCode == HttpURLConnection.HTTP_OK){
+					InputStream inputStream = connection.getInputStream();
+					Reader reader = new InputStreamReader(inputStream);
+					int contentLength = connection.getContentLength();
+					char[] charArray = new char[contentLength];
+					reader.read(charArray);
+					//Store JSON Data
+					String responseData = new String(charArray);
+					
+					jsonResponse = new JSONObject(responseData);
+				}
+				else{
+					Log.i(TAG, "Unsuccessful HTTP Response Code: " + responseCode);
+				}
+				
+				Log.i(TAG, "Code:" +  responseCode);
+			} 
+			catch (MalformedURLException e) {
+				Log.e(TAG, "Exception caught", e);
+			}
+			catch (IOException e){
+				Log.e(TAG, "Exception caught", e);
+			}
+			catch (Exception e){
+				Log.e(TAG, "Exception caught", e);
+			}
+			return jsonResponse;
+		}
+		@Override
+		protected void onPostExecute(JSONObject result){
+			mBlogData = result;
+			updateList();
+		}
 	}
 
 	@Override
